440c2d74f4be731c23a588c6b2a1b77c8fad41f5
Rakwin <lhajjirakwin@gmail.com>
2023-06-21 15:52:05 -0700

    alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,8 +2,8 @@
 
 
 	/**
-	 * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
-	 * followed by a new line
+	 * print_alphabet_x10 - Make alphabet x10 times
+	 * Return: void
 	 */
 	void print_alphabet_x10(void)
 	{
@@ -16,12 +16,10 @@
 
 		while (i < 10)
 		{
-			ch = 'a';
-			while (ch <= 'z')
-			{
-				_putchar(ch);
-				ch++;
-			}
+		for	(ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
 			_putchar('\n');
 			i++;
 		}
